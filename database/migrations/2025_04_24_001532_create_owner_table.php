332f1857fbd07d4e5225fee294d6e2601e2ab61f
alves-Moises <moisesinho_ra@hotmail.com>
2025-04-24 00:09:14 -0300

    first commit

---
+++ database/migrations/2025_04_24_001532_create_owner_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('owner', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('name');
+            $table->string('tel');
+            $table->unsignedBigInteger('establishment_id');
+            $table->foreign('establishment_id')
+                ->references('id')
+                ->on('Establishment')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+        #TODO: rest of tables before migrategit
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('owner');
+    }
+};
